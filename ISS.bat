902d24a56a25b169fd7462baa76afe2998530565
stevendodd <stevendodd@outlook.com>
2024-04-01 11:21:44 +0100

    Added Tone to ISS

--- ISS.bat
+++ ISS.bat
@@ -1,2 +1,2 @@
-python main.py --toneFrequency 67 --satBand SUB --opMode FM --dataMode OFF
+python main.py --tone TONE --toneFrequency 67 --satBand SUB --opMode FM --dataMode OFF
 python main.py --satBand MAIN --opMode FM --dataMode OFF
